872a9e49f6e97fb85263d9bef8d666285bb5c3d5
Caique <caique@Gingadmins-Mac-mini.local>
2018-04-11 14:44:25 -0300

    implemented flight-service

--- flight-service/src/main/java/com/flight/flightservice/model/Airplane.java
+++ flight-service/src/main/java/com/flight/flightservice/model/Airplane.java
@@ -5,19 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 @Data
 @Document
 @RequiredArgsConstructor
-public class Flight {
+public class Airplane {
 
     private @Id UUID id = UUID.randomUUID();
-    private final Integer flightCode;
-    private final LocalDateTime departureDateTime;
-    private final LocalDateTime arrivalDateTime;
-    private final Airplane airplane;
-    private final Pilot pilot;
+    private String airplaneModel;
+    private String airplaneCode;
 }
--- flight-service/src/main/java/com/flight/flightservice/model/Pilot.java
+++ flight-service/src/main/java/com/flight/flightservice/model/Pilot.java
@@ -5,19 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 @Data
 @Document
 @RequiredArgsConstructor
-public class Flight {
+public class Pilot {
 
     private @Id UUID id = UUID.randomUUID();
-    private final Integer flightCode;
-    private final LocalDateTime departureDateTime;
-    private final LocalDateTime arrivalDateTime;
-    private final Airplane airplane;
-    private final Pilot pilot;
+    private String pilotName;
+    private String pilotDocument;
+
 }
